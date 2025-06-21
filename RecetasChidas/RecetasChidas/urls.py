f53537c9365b34cbde9a321d83dad8aaac4c6a37
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-06-20 18:37:21 -0600

    Agregada ruta para logout

--- RecetasChidas/RecetasChidas/urls.py
+++ RecetasChidas/RecetasChidas/urls.py
@@ -21,5 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.presentation.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='tienda/login.html'), name='login')
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='tienda/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout')
 ]
